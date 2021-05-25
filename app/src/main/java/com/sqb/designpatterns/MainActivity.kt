568c82f5e1c0539af503f144b09bb0b7a8375a28
sqb <sqbfine@163.com>
2021-05-25 16:44:32 +0800

    :tada: kotlin开始项目

--- app/src/main/java/com/sqb/designpatterns/MainActivity.kt
+++ app/src/main/java/com/sqb/designpatterns/MainActivity.kt
@@ -2,10 +2,23 @@ package com.sqb.designpatterns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.sqb.designpatterns.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        init()
+    }
+
+    private fun init() {
+        binding.bt1.setOnClickListener {
+            Toast.makeText(this, "hhhh"+it.height, Toast.LENGTH_SHORT).show()
+        }
     }
 }
